19a88d3a42f21e237a24e0ce680e836dd9664e8d
dwieb <dwayne.harmon@gmail.com>
2019-07-08 19:18:28 -0500

    Great updates team\!

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# User.create!()
+
+User.create!(name: "Terry", password: "password")
+
+10.times do
+  Project.create!(title: Faker::Superhero.name, user_id: 1)
+end
