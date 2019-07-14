526da2198b750b3eb7e758a594f54b7dc42a874e
github username <github email address>
2019-07-14 14:03:09 -0400

    add nested favorite route to project resource

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,16 @@ class User < ApplicationRecord
   has_many :projects
   has_many :favorite_projects
   has_many :favorites, through: :favorite_projects, source: :project
-  
+
+  def favorited!(project)
+    self.favorites << project
+  end
+
+  def unfavorited!(project)
+    self.favorites.delete(project)
+  end
+
+  def favorited?(project)
+    self.favorites.find_by(id: project.id).present?
+  end
 end
