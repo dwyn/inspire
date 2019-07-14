c0ba4bc5c3d03969d5a8b7df5eeb029af6dead63
github username <github email address>
2019-07-14 13:45:24 -0400

    add favorite_projects associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :projects
+  has_many :favorite_projects
+  has_many :favorites, through: :favorite_projects, source: :project
+  
 end
