765450f8c3102fc9f3ff50d42f59f45f4bf7b1d5
dwieb <dwayne.harmon@gmail.com>
2019-07-15 22:52:07 -0500

    Cleaned up sessions controller, and login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,27 +1,25 @@
 class SessionsController < ApplicationController
 
     def new
-        # @user = User.new
+      @user = User.new
     end
 
     def create
-        user = User.find_by(email: params[:session][:email].downcase) 
-        if user && user.authenticate(params[:session][:password]) 
-            session[:user_id] = user.id 
-            flash[:success] = "You have successfully logged in" 
-            redirect_to user 
+        user = User.find_by(email: params[:email])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            flash[:success] = "You have successfully logged in"
+            redirect_to user
         else
-            flash.now[:danger] = "There was a problem with your login information." 
-            render "new" 
+            flash.now[:danger] = "There was a problem with your login information."
+            render "new"
         end
     end
 
     def destroy
-        session[:user_id] 
-        session[:user_id] = nil 
-        flash[:success] = "You have logged out" 
-        redirect_to root_path 
+        session[:user_id]
+        session[:user_id] = nil
+        flash[:success] = "You have logged out"
+        redirect_to root_path
     end
-
-
 end
